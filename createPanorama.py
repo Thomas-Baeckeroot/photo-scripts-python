58bb8c69c1ab6bc7cb6c9de9d4226e4fa64f14fb
Nihagaflacs <tbaecker.spam@gmail.com>
2016-09-13 10:11:16 -0300

    Create (empty) createPanorama.py
    Empty createPanorama.py as base file.

---
+++ createPanorama.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+# Aim of this script is to create automatically a panorama from a set of
+# pictures (separated in a folder).
+# Folder containing the pictures is the only parameter of this script.
+#
+# Note:
+# This script is written
+# in Python:		tested with 2.7
+# for running in Linux:	Mint 16
+# with Hungin:		v2011.4.0
+###############################################################################
+# Date		Author			Modification description
+#------------------------------------------------------------------------------
+# 2014-09-14	Thomas BAECKEROOT	CREATION
+#		(tbaecker.spam@gmail.com)
+#------------------------------------------------------------------------------
+#
+###############################################################################
